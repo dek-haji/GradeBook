ff82d07e2c71a09bee35c0b7bfbae6e6642e1e47
dek-haji <deeeqhaji@gmail.com>
2021-01-10 16:52:19 -0600

    deleted the numbers array and using List

--- Program.cs
+++ Program.cs
@@ -8,13 +8,9 @@ namespace Gradebook
         static void Main(string[] args)
         {
             //to initialize a property you have to use the  new keyword
-            var numbers = new[] {12.7, 34.1, 32.3};
             //using List
             //List requires 1 type of argument.
-            var grades = new List<double>();
-            grades.Add(12.7);
-            grades.Add(13.7);
-            grades.Add(32.7);
+            var grades = new List<double>() {12.7, 34.1, 32.3};
             grades.Add(43.1);
             var result = 0.0;
             foreach (var number in grades)
