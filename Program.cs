9b813123e9e3ac0f5ab1e3d5cd10d9663b73965f
dek-haji <deeeqhaji@gmail.com>
2021-01-10 16:28:16 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //to initialize a property you have to use the  new keyword
+            var numbers = new[] {12.7, 34.2, 32.3};
+            //using List
+           // List grades;
+
+            var result = 0.0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
