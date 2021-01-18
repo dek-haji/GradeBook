ac8c829fcaac4b3653858433fae824fa5a14f182
dek-haji <deeeqhaji@gmail.com>
2021-01-17 18:06:09 -0600

    encapsulated the object

--- Book.cs
+++ Book.cs
@@ -5,10 +5,32 @@ namespace Gradebook
 {
     class Book
     {
+          List<double> grades = new List<double>();
+        private string name;
         //creating a cnstructor method
-        public Book()
+        public Book(string name)
         {
             var grades = new List<double>();
+            this.name = name;
+        }
+        public void ShowStatistics(Book book)
+        {
+            var grades = new List<double>() { 12.7, 34.1, 32.3 };
+        grades.Add(43.1);
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+               highGrade = Math.Max(number, highGrade);
+               lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+    result /= grades.Count;
+            //we using N1 to get only the first digit after the decemal.
+            Console.WriteLine($" The average grade of the students is {result:N1}");
+            Console.WriteLine($"The lowest number is {lowGrade}");
+            Console.WriteLine($"The Highest number is {highGrade}");
         }
         public void AddGrade(double grade) {
                if(grade >= 0) {
@@ -16,7 +38,5 @@ namespace Gradebook
                    Console.WriteLine($"the new grade is {grade}");
                }
         }
-        List<double> grades = new List<double>();
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,29 +7,18 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            var book2 = new Book();
+            var book2 = new Book("Dek Haji's book");
             var name = new Names();
-            var book = new Book();
+            var book = new Book("Nasri's book");
             book2.AddGrade(32.4);
             book.AddGrade(33.2);
             name.AddName("Sahra");
 
-            if(book2 == null) {
-                Console.WriteLine("its not working because is a null");
-            }
+            book.ShowStatistics(book2);
             //to initialize a property you have to use the  new keyword
         //using List
         //List requires 1 type of argument.
-        var grades = new List<double>() { 12.7, 34.1, 32.3 };
-        grades.Add(43.1);
-            var result = 0.0;
-            foreach (var number in grades)
-            {
-                result += number;
-            }
-    result /= grades.Count;
-            //we using N1 to get only the first digit after the decemal.
-            Console.WriteLine($" The average grade of the students is {result:N1}");
-        }
+        
     }
+}
 }
